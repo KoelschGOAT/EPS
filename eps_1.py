56cda94aebdcdbb03e5fd5e15ec301cc9a3a7ae1
Luca <luca.duepmann@gmail.com>
2021-06-13 17:52:42 +0200

    test2

--- eps_1.py
+++ eps_1.py
@@ -3,37 +3,38 @@ import os
 import time
 import keyboard
 
+
 class EPSAccount(Account):
     _account_list = []
     _balance_list = []
+
     def create_account(self):
         num = Account.create_number(self)
         pin = Account.create_pin(self)
-        self._account_list.append((num,pin))
-        self._balance_list.append((num,self.balance))
-        #print(f"Accounts: {self._account_list}")
-        #print(f"Account balances: {self._balance_list}")
+        self._account_list.append((num, pin))
+        self._balance_list.append((num, self.balance))
+        # print(f"Accounts: {self._account_list}")
+        # print(f"Account balances: {self._balance_list}")
         os.system('cls')
         print(f"""Account created:
 Number: {num}
 Pin: {pin}
 """)
-        
-    
+
     def Login(self):
         self.login_num = ""
         if len(self._account_list) > 0:
             while True:
                 num_input = input("Please Input your Account Number: ")
                 pin_input = input("Please Input your Pin: ")
-                #Compare input to existing Accounts in Account List
+                # Compare input to existing Accounts in Account List
                 for index, item in enumerate(self._account_list):
                     if item[0] == num_input:
                         if item[1] == pin_input:
                             print("Login Login successfull")
                             self.login_num = num_input
                             return True
-                        
+
                 print("Login failed, Account Details invalid")
         print("No Account created")
         time.sleep(2)
@@ -43,10 +44,10 @@ Pin: {pin}
         if self.login_num != "":
             for index, item in enumerate(self._balance_list):
                 if item[0] == self.login_num:
-                   print(f"Your current Balance is: {item[1]}\n")
-                   time.sleep(2)
-                   return
-        
+                    print(f"Your current Balance is: {item[1]}\n")
+                    time.sleep(2)
+                    return
+
     def main(self):
         end1 = True
         end2 = True
@@ -92,6 +93,8 @@ Pin: {pin}
                 quit()
             else:
                 print("The Input is invalid")
+
+
 if __name__ == '__main__':
     a = EPSAccount()
     a.main()
