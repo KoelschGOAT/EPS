cf601dad8f006aa2d95a7b9372adc249da2603a8
Luca Düpmann <luca.duepmann@gmail.com>
2021-06-21 20:55:37 +0200

    transactions

--- eps_1.py
+++ eps_1.py
@@ -1,23 +1,32 @@
-from esp_base import Account
+from eps_base import Account
+from database import Database
 import os
 import time
-
+import sqlite3
 
 class EPSAccount(Account):
-    _account_list = []
-    _balance_list = []
+    def __init__(self):
+        super().__init__()
+        self.login_num = ""
+        self.login_pin = ""
+        self.menu1 = """Willkommen in EPS:
+    1. Create Account
+    2. Login Account
+    0. Exit
+
+    >> """
+        self.menu2 = f"""Logged in as: {self.login_num}
+    1. show balance
+    2. Logout
+    0. Exit
+
+     >> """
 
     def create_account(self):
-        num = Account.create_number(self)
-        pin = Account.create_pin(self)
-        num = num.strip()
-        pin = pin.strip()
-        # Create a Tuple inside list with number and pin
-        self._account_list.append((num, pin))
-        self._balance_list.append((num, self.balance))
-        # print(f"Accounts: {self._account_list}")
-        # print(f"Account balances: {self._balance_list}")
-        os.system('cls')
+        num = super().create_number()
+        pin = super().create_pin()
+        super().add_user_to_account(num, pin, super().show_balance())
+        
         print(f"""Account created:
 Number: {num}
 Pin: {pin}
@@ -25,65 +34,51 @@ Pin: {pin}
 
     def Login(self):
         self.login_num = ""
-        if len(self._account_list) > 0:
-            try:
-                while True:
-                    num_input = input("Please Input your Account Number: ")
-                    pin_input = input("Please Input your Pin: ")
-                    # Compare input to existing Accounts in Accounts List
-                    for index, item in enumerate(self._account_list):
-                        if item[0] == num_input or int(item[0]) == int(num_input):
-                            if item[1] == pin_input:
-                                print("Login Login successfull")
-                                self.login_num = num_input.zfill(8)
-                                return True
-                    print("Login failed, Account Details invalid")
-
-            except Exception:
-                print("invalid Input")
-        print("No Account created")
+        while True:
+            num = input("Please Input your Account Number: ")
+            pin = input("Please Input your Pin: ")
+            num = num.strip()
+            pin = pin.strip()
+            # Compare input to existing Accounts in Accounts List
+            if super().login(num, pin).fetchone():
+                print("Login Successfully")
+                self.login_pin = pin
+                self.login_num = num
+                return True
+            else:
+                print("Login failed, Account Details invalid")
+ 
         time.sleep(2)
-        os.system('cls')
+         
 
     def Balance(self):
-        if self.login_num != "":
-            for index, item in enumerate(self._balance_list):
-                if item[0] == self.login_num:
-                    print(f"Your current Balance is: {item[1]}\n")
-                    time.sleep(2)
-                    return
+        self.balance = super().get_balance(self.login_num, self.login_pin).fetchall()
+        print(f"Your current Balance is: {self.balance[0][0]}€\n")
+        time.sleep(2)
+        return
 
     def main(self):
+        super().create_table()
         end1 = True
         end2 = True
         while end1:
-            menu_input = input("""Willkommen in EPS:
-    1. Create Account
-    2. Login Account
-    0. Exit
-
-    >> """)
+            menu_input = input(self.menu1)
             if menu_input == "1":
                 self.create_account()
                 continue
             elif menu_input == "2":
                 if self.Login():
                     time.sleep(1)
-                    os.system('cls')
+                     
                     while end2:
-                        menu2_input = input(f"""Logged in as: {self.login_num}
-    1. show balance
-    2. Logout
-    0. Exit
-
-     >> """)
+                        menu2_input = input(self.menu2)
                         if menu2_input == "1":
                             self.Balance()
                             continue
                         elif menu2_input == "2":
                             print("You've been logged out")
                             time.sleep(1)
-                            os.system('cls')
+                             
                             end2 = False
                         elif menu2_input == "0":
                             os.system("cls")
@@ -99,7 +94,11 @@ Pin: {pin}
             else:
                 print("The Input is invalid")
 
-
+        
+        
 if __name__ == '__main__':
+
     a = EPSAccount()
+    
+    a.balance = 49.99
     a.main()
